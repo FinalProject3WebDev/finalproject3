f8b94ab40e34094ec5ac1c4081eef58f66396b00
Sovia A Kusuma <agustinasovia@gmail.com>
2023-10-01 22:04:54 +0700

    prodComponent ts

--- Frontend/src/app/product/product.component.ts
+++ Frontend/src/app/product/product.component.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../interfaces/product';
+import { ProductCategory } from '../interfaces/productCategory';
 import { ProductService } from '../services/product.service';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { ProductCategoryService } from '../services/product-category.service';
 
 @Component({
   selector: 'app-product',
@@ -10,13 +13,33 @@ import { ProductService } from '../services/product.service';
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [];
+  categories: ProductCategory[] = [];
+  selectedCategory: ProductCategory | null = null;
+
   quantity: number = 1;
+  faShoppingCart = faShoppingCart;
   
-  constructor(private http: HttpClient, private productService: ProductService) { }
+  constructor(
+    private http: HttpClient, 
+    private productService: ProductService,
+    private productCategoryService: ProductCategoryService
+  ) { }
 
   ngOnInit(): void {
-    this.productService.getAllProducts().subscribe((data: any) => {
+    this.productService.getAllProducts().subscribe((data: Product[]) => {
       this.products = data;
+      console.log(this.products)
+    });
+
+    this.productCategoryService.getCategories().subscribe((data: ProductCategory[]) => {
+      this.categories  = data;
+    });
+  }
+
+  getProductsByCategory(category: ProductCategory) {
+    this.selectedCategory = category; 
+    this.productService.getProductsByCategory(category.id.toString()).subscribe((data: any) => {
+      const firstProduct = data[0];
     });
   }
 
